f7ec046da2ce18c551a82beb466bb66cd853b335
BaBa0525 <103883300+BaBa0525@users.noreply.github.com>
2023-11-16 23:56:59 +0800

    feat: prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,15 +15,21 @@ const config = {
     "plugin:react-hooks/recommended",
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
+    "prettier",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-    project: ["./tsconfig.json", "./tsconfig.node.json"],
-    tsconfigRootDir: __dirname,
-  },
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        project: ["./tsconfig.json", "./tsconfig.node.json"],
+        tsconfigRootDir: __dirname,
+      },
+    },
+  ],
   plugins: ["react-refresh"],
   rules: {
     "react-refresh/only-export-components": [
