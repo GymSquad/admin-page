b111d1464341518fb556e753ad94fdfeac7d0584
BaBa0525 <103883300+BaBa0525@users.noreply.github.com>
2023-11-17 00:02:11 +0800

    feat: tailwindcss

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,4 +1,10 @@
-/** @type {import("prettier").Config} */
-const config = {};
+/** @typedef {import("prettier").Config} PrettierConfig */
+/** @typedef {import("prettier-plugin-tailwindcss").PluginOptions} TailwindConfig */
+
+/** @type {PrettierConfig & TailwindConfig} */
+const config = {
+  plugins: ["prettier-plugin-tailwindcss"],
+  tailwindConfig: "./tailwind.config.ts",
+};
 
 export default config;
