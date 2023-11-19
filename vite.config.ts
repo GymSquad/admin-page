40c7a0c6293f084fc6488a75831ddd05296b49d8
BaBa0525 <103883300+BaBa0525@users.noreply.github.com>
2023-11-19 23:15:05 +0800

    feat: init shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { defineConfig } from "vite";
 import pages from "vite-plugin-pages";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   plugins: [
     react(),
     pages({
