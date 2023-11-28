5d1a760099e33e28bef0c92af423e91604408aa6
BaBa0525 <103883300+BaBa0525@users.noreply.github.com>
2023-11-29 00:19:47 +0800

    chore: env setting

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import createClient from "openapi-fetch";
 import { paths } from "./types";
 
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const apiClient = createClient<paths>({
   baseUrl: BASE_URL,
