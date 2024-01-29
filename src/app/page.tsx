c581d4e6cfb2f7b8dbddaa83a93d1193c076dd0d
BaBa0525 <103883300+BaBa0525@users.noreply.github.com>
2024-01-29 22:41:53 +0800

    feat: debounce search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,42 @@
 import { WebsiteInfoTable } from "@/components/websites/WebsiteInfoTable";
 import { useWebsiteInfo } from "@/components/websites/query";
+import { useDebounce } from "@/lib/utils";
 import { Search } from "lucide-react";
 import { useState } from "react";
 
 const HomePage = () => {
-  const [query, setQuery] = useState("");
-  const webInfoQuery = useWebsiteInfo({ q: query });
+  const [searchText, setSearchText] = useState("");
+
+  const webInfoQuery = useWebsiteInfo({ q: searchText });
 
   return (
-    <div className="container mx-auto flex h-screen flex-col gap-4 py-10">
-      <div className="flex items-center gap-2 rounded-full border border-input px-4 focus-within:ring-2 focus-within:ring-ring">
-        <Search />
-        <input
-          placeholder="Search"
-          className="h-full w-full py-2 text-lg focus-visible:outline-none"
-          value={query}
-          onChange={(e) => {
-            setQuery(e.target.value);
-          }}
-        />
-      </div>
+    <div className="container mx-auto flex h-screen max-w-6xl flex-col gap-4 py-10">
+      <SearchBar setSearchText={setSearchText} />
       <WebsiteInfoTable queryRes={webInfoQuery} />
+    </div>
+  );
+};
+
+type SearchBarProps = {
+  setSearchText: (text: string) => void;
+};
+
+const SearchBar = ({ setSearchText }: SearchBarProps) => {
+  const [inputText, setInputText] = useState("");
+  const debounceSearch = useDebounce(setSearchText, 1000);
+
+  return (
+    <div className="flex items-center gap-2 rounded-full border border-input px-4 focus-within:ring-2 focus-within:ring-ring">
+      <Search />
+      <input
+        placeholder="Search"
+        className="h-full w-full py-2 text-lg focus-visible:outline-none"
+        value={inputText}
+        onChange={(e) => {
+          setInputText(e.target.value);
+          debounceSearch(e.target.value);
+        }}
+      />
     </div>
   );
 };
