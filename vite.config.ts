d6ca14e2e910cc99786c278fb03ada90b1137279
BaBa0525 <103883300+BaBa0525@users.noreply.github.com>
2024-03-03 16:42:25 +0800

    dep: init shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { fileURLToPath } from "node:url";
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import react from "@vitejs/plugin-react-swc";
+import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   resolve: {
     alias: {
-      "~": fileURLToPath(new URL("./src", import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
   plugins: [react(), TanStackRouterVite({ quoteStyle: "double" })],
