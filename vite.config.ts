34f9666ab9e3bc417e0c166bd00271d47416e994
BaBa0525 <103883300+BaBa0525@users.noreply.github.com>
2024-03-03 17:03:35 +0800

    fix: enable code action save using biome

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
+import { fileURLToPath } from "node:url";
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import react from "@vitejs/plugin-react-swc";
-import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 
 export default defineConfig({
